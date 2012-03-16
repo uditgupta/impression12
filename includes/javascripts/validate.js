5d56c8e1c16253c3719d11d9cc3ae4ea7a652281
Neeraj Khandelwal <nk.zscorpion@gmail.com>
2012-03-16 22:33:59 +0530

    Latest

--- includes/javascripts/validate.js
+++ includes/javascripts/validate.js
@@ -11,13 +11,10 @@ function check(id){
 function validate(){
 	var n = check('name');
 	var d = check('details');
-	var r = check('rules');
-	var v = check('venue');
-	var t = check('time');
 	var c = check('category');
 	var ty = check('type');
 	var u = check('hidid');
-	if(n && d && r && v && t && c && ty){
+	if(n && d && t && c && ty){
 		if(!u){
 			alert("We are currently unable to register your event. Please come back later or try refreshing the page.");
 			return false;
